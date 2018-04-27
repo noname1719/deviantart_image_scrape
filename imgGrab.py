09a2784a33e460558e5ed4e880130b97c54ccd1e
Reimu <yunihikokun@gmail.com>
2018-04-27 05:31:20 +0500

    w

--- imgGrab.py
+++ imgGrab.py
@@ -1,11 +1,24 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-html = urlopen('https://www.deviantart.com/popular-24-hours/')
-soup = BeautifulSoup(html, 'html.parser')
-links = soup.find_all('a', 'torpedo-thumb-link')
-for i in links:
-    print(i['href'])
+
+def steal_image(links):
+    pass
 
 
+def collect_links(tag, max_count):
+    big_black_bag = set()
+    offset = 0
+    while True:
+        html = urlopen('https://www.deviantart.com/?q=' + tag + '&offset=' + str(offset))
+        soup = BeautifulSoup(html, 'html.parser')
+        links = soup.find_all('a', 'torpedo-thumb-link')
+        for link in links:
+            big_black_bag.add(link['href'])
+            if len(big_black_bag) == max_count:
+                return big_black_bag
+        offset += 24
 
+
+for i in collect_links('mlp', 54):
+    print(i)
